ed489ff1e0e2bf8a2e4fe325b7b3a4946e5fb456
KillerFrca <killerfrca@himym.cz>
2010-09-12 18:30:24 +0200

    Revert "moc to teda nechapu ale todle by mohlo pomoct"
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -8898,8 +8898,7 @@ void Aura::PeriodicDummyTick()
                 // Killing Spree
                 case 51690:
                 {
-                    Unit* caster = GetCaster();
-                    if (!caster || caster->hasUnitState(UNIT_STAT_STUNNED) || caster->isFeared())
+                    if (target->hasUnitState(UNIT_STAT_STUNNED) || target->isFeared())
                         return;
 
                     std::list<Unit*> targets;
@@ -8907,9 +8906,9 @@ void Aura::PeriodicDummyTick()
                         // eff_radius ==0
                         float radius = GetSpellMaxRange(sSpellRangeStore.LookupEntry(spell->rangeIndex));
 
-                        MaNGOS::AnyUnfriendlyVisibleUnitInObjectRangeCheck u_check(caster, caster, radius);
-                        MaNGOS::UnitListSearcher<MaNGOS::AnyUnfriendlyVisibleUnitInObjectRangeCheck> checker(caster, targets, u_check);
-                        Cell::VisitAllObjects(caster, checker, radius);
+                        MaNGOS::AnyUnfriendlyVisibleUnitInObjectRangeCheck u_check(target, target, radius);
+                        MaNGOS::UnitListSearcher<MaNGOS::AnyUnfriendlyVisibleUnitInObjectRangeCheck> checker(target, targets, u_check);
+                        Cell::VisitAllObjects(target, checker, radius);
                     }
 
                     if (targets.empty())
@@ -8919,8 +8918,8 @@ void Aura::PeriodicDummyTick()
                     std::advance(itr, rand()%targets.size());
                     Unit* victim = *itr;
 
-                    caster->CastSpell(victim, 57840, true);
-                    caster->CastSpell(victim, 57841, true);
+                    target->CastSpell(victim, 57840, true);
+                    target->CastSpell(victim, 57841, true);
                     return;
                 }
                 default:
