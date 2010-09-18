9581a6afc5455961a5c7bdcbf747f56668e76b35
unknown <vbocek@gmail.com>
2010-09-18 15:31:37 +0200

    [pr779] CMSG_NAME_QUERY can be sent before client log in, add lookup for pet when player is teleporting (= not in map) in CMSG_PET_NAME_QUERY

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10272"
- #define REVISION_VP "pr778"
+ #define REVISION_VP "pr779"
  #define REVISION_VP_DATE "18.9.2010"
 #endif // __REVISION_NR_H__
