3d8441838d9306622ede508df3c47b6451ce6d37
reeshack <richard.galis@gmail.com>
2010-09-19 04:57:03 +0200

    [pr782] This should fix Lifebloom heal/mana return at dispel

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1429,6 +1429,25 @@ bool Aura::modStackAmount(int32 num)
     if (!m_spellProto->StackAmount)
         return true;
 
+    // Lifebloom - this is needed for proper heal/mana return because when is m_stackAmount set to 0 it will never pass through the "&& GetStackAmount() > 0" condition in its dummy aura...
+    if (m_spellProto->SpellFamilyName != SPELLFAMILY_DRUID && !(m_spellProto->SpellFamilyFlags & UI64LIT(0x1000000000)))
+    {
+        if (Unit *target = GetTarget())
+        {
+            // final heal, only when removing stacks
+            if (target->IsInWorld() && num < 0)
+            {
+                //Heal
+                target->CastCustomSpell(target, 33778, &m_modifier.m_amount, NULL, NULL, true, NULL, this, GetCasterGUID());
+                //Return mana
+                if (Unit* caster = GetCaster())
+                {
+                    int32 returnmana = (GetSpellProto()->ManaCostPercentage * caster->GetCreateMana() / 100) * (-(num) / 2);
+                    caster->CastCustomSpell(caster, 64372, &returnmana, NULL, NULL, true, NULL, this, GetCasterGUID());
+                }
+            }
+        }
+    }
     // Modify stack but limit it
     int32 stackAmount = m_stackAmount + num;
     if (stackAmount > (int32)m_spellProto->StackAmount)
@@ -3065,7 +3084,7 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                 else
                 {
                     // Final heal only on dispelled or duration end
-                    if (!(GetAuraDuration() <= 0 || m_removeMode == AURA_REMOVE_BY_DISPEL))
+                    if (!(GetAuraDuration() <= 0/* || m_removeMode == AURA_REMOVE_BY_DISPEL*//*lifebloom dispel has special handling in modStackAmount()*/))
                         return;
 
                     // have a look if there is still some other Lifebloom dummy aura
