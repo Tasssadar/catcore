4af050b5f882001546c9fadef17094c45d13a595
reeshack <richard.galis@gmail.com>
2010-09-19 03:22:51 +0200

    [pr780] hack na Warrior T9 protection 4P bonus

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10272"
- #define REVISION_VP "pr779"
- #define REVISION_VP_DATE "18.9.2010"
+ #define REVISION_VP "pr780"
+ #define REVISION_VP_DATE "19.9.2010"
 #endif // __REVISION_NR_H__
