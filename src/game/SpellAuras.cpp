151345e637022dfaef794b75949a47feb38bdc77
reeshack <richard.galis@gmail.com>
2010-09-20 20:27:43 +0200

    TYPOOOO
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1430,7 +1430,7 @@ bool Aura::modStackAmount(int32 num)
         return true;
 
     // Lifebloom - this is needed for proper heal/mana return because if m_stackAmount is set to 0 it will never pass through the "&& GetStackAmount() > 0" condition in its dummy aura...
-    if (m_spellProto->SpellFamilyName != SPELLFAMILY_DRUID && !(m_spellProto->SpellFamilyFlags & UI64LIT(0x1000000000)))
+    if (m_spellProto->SpellFamilyName == SPELLFAMILY_DRUID && (m_spellProto->SpellFamilyFlags & UI64LIT(0x1000000000)))
     {
         if (Unit *target = GetTarget())
         {
