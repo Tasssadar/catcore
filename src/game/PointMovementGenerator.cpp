bdc4414f8c017df4faf3bc4523d035aa746dd337
unknown <vbocek@gmail.com>
2010-10-02 01:19:00 +0200

    [10420] Avoid send stop packet in PointMovementGenerator when unit is not moving
    Also never send stop packet when destinationHolder hasArrived, it's expected that unit is already not moving.
    
    Signed-off-by: NoFantasy <nofantasy@nf.no>
    
    Conflicts:
    
    	src/shared/revision_nr.h

--- src/game/PointMovementGenerator.cpp
+++ src/game/PointMovementGenerator.cpp
@@ -28,8 +28,11 @@
 template<class T>
 void PointMovementGenerator<T>::Initialize(T &unit)
 {
-    unit.StopMoving();
+    if (!unit.IsStopped())
+        unit.StopMoving();
+
     unit.addUnitState(UNIT_STAT_ROAMING|UNIT_STAT_ROAMING_MOVE);
+
     Traveller<T> traveller(unit);
     i_destinationHolder.SetDestination(traveller, i_x, i_y, i_z);
 
@@ -53,10 +56,11 @@ void PointMovementGenerator<T>::Interrupt(T &unit)
 template<class T>
 void PointMovementGenerator<T>::Reset(T &unit)
 {
-    unit.StopMoving();
+    if (!unit.IsStopped())
+        unit.StopMoving();
+
     unit.addUnitState(UNIT_STAT_ROAMING|UNIT_STAT_ROAMING_MOVE);
 }
-
 
 template<class T>
 bool PointMovementGenerator<T>::Update(T &unit, const uint32 &diff)
@@ -71,6 +75,7 @@ bool PointMovementGenerator<T>::Update(T &unit, const uint32 &diff)
     }
 
     unit.addUnitState(UNIT_STAT_ROAMING_MOVE);
+
     Traveller<T> traveller(unit);
     if (i_destinationHolder.UpdateTraveller(traveller, diff, false))
     {
@@ -80,7 +85,7 @@ bool PointMovementGenerator<T>::Update(T &unit, const uint32 &diff)
 
     if (i_destinationHolder.HasArrived())
     {
-        unit.StopMoving();
+        unit.clearUnitState(UNIT_STAT_ROAMING_MOVE);
         MovementInform(unit);
         return false;
     }
