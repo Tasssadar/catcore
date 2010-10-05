169148f8343b4be91daa1045f93178fd6762a162
KillerFrca <killerfrca@gmail.com>
2010-10-05 17:30:36 +0200

    [pr818] Mixology omfg pyco
    Signed-off-by: KillerFrca <killerfrca@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10552"
- #define REVISION_VP "pr817"
- #define REVISION_VP_DATE "4.10.2010"
+ #define REVISION_VP "pr818"
+ #define REVISION_VP_DATE "5.10.2010"
 #endif // __REVISION_NR_H__
