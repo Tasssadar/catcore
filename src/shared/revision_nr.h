45d503d16379b88adb9879e4fbba786d0cced67e
unknown <vbocek@gmail.com>
2010-10-11 18:41:18 +0200

    [pr841] Set Debug log instead of error_log in HandleItemNameQueryOpcode()

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10552"
- #define REVISION_VP "pr840"
+ #define REVISION_VP "pr841"
  #define REVISION_VP_DATE "11.10.2010"
 #endif // __REVISION_NR_H__
