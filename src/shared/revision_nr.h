e9fe46a465749601b0bf233f66f7f8ada396ac83
KillerFrca <killerfrca@gmail.com>
2010-10-13 19:06:37 +0200

    [pr844] Jen tak  :-)
    Signed-off-by: KillerFrca <killerfrca@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10552"
- #define REVISION_VP "pr843"
+ #define REVISION_VP "pr844"
  #define REVISION_VP_DATE "12.10.2010"
 #endif // __REVISION_NR_H__
