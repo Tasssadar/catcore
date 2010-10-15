d89a28762b4cbdd44b2d3a89f312d545e99cec38
reeshack <richard.galis@gmail.com>
2010-10-15 14:25:14 +0200

    [pr847] Draenei racial passive (hit) stacking
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10552"
- #define REVISION_VP "pr846"
+ #define REVISION_VP "pr847"
  #define REVISION_VP_DATE "15.10.2010"
 #endif // __REVISION_NR_H__
