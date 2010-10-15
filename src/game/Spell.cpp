de277ad793221982101c5944d8b62beb7637eeca
reeshack <richard.galis@gmail.com>
2010-10-16 01:50:27 +0200

    [pr848] Frost armor/Ice armor triggered should be able to trigger
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -712,6 +712,9 @@ void Spell::prepareDataForTriggerSystem()
                 // Replenish Mana, item spell with triggered cases (Mana Agate, etc mana gems)
                 else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x0000010000000000))
                     m_canTrigger = true;
+                // Frost armor/Ice armor
+                else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x0000000000100000))
+                    m_canTrigger = true;
                 break;
             case SPELLFAMILY_WARLOCK:
                 // For Hellfire Effect / Rain of Fire / Seed of Corruption triggers need do it
