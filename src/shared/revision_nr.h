559ab05dc3dbde7cb65cf7e43abf555c16742fbf
reeshack <richard.galis@gmail.com>
2010-11-25 15:30:30 +0100

    [pr962] Some fixes for Chimera Shot
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10707"
- #define REVISION_VP "pr961"
+ #define REVISION_VP "pr962"
  #define REVISION_VP_DATE "25.11.2010"
 #endif // __REVISION_NR_H__
