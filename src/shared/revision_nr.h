943b2ee3702bcb0aa24ef5d4753684a3a4da631e
Tassadar <vbocek@gmail.com>
2010-12-04 13:11:51 +0100

    Add some shiny things to .s in

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -2,5 +2,6 @@
 #define __REVISION_NR_H__
  #define REVISION_NR "10707"
  #define REVISION_VP "pr981"
+ #define REVISION_TOURNAMENT "3"
  #define REVISION_VP_DATE "1.12.2010"
 #endif // __REVISION_NR_H__
