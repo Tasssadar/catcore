a7022ebccbc6d431cb5168fd4c21ddcf69186c1c
reeshack <richard.galis@gmail.com>
2010-12-05 14:06:19 +0100

    [pr994] Blood Draining proc
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10707"
- #define REVISION_VP "pr993"
+ #define REVISION_VP "pr994"
  #define REVISION_VP_DATE "5.12.2010"
 #endif // __REVISION_NR_H__
