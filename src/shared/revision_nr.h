cb1201392cde626c146e0482faa5ff6f35485819
Tassadar <vbocek@gmail.com>
2010-12-12 12:13:03 +0100

    revka

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10707"
- #define REVISION_VP "pr1002"
+ #define REVISION_VP "pr10014"
  #define REVISION_TOURNAMENT "18"
  #define REVISION_VP_DATE "11.12.2010"
 #endif // __REVISION_NR_H__
