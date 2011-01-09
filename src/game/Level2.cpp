c8d05c7c90c6265cb2334ec395817c339948b752
KillerFrca <killerfrca@himym.cz>
2011-01-09 11:37:38 +0100

    [pr1034] Osefeni toho prikazu na dveře
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/game/Level2.cpp
+++ src/game/Level2.cpp
@@ -670,8 +670,14 @@ bool ChatHandler::HandleGameObjectClickCommand(const char* args)
         return false;
     }
 
+    if (pGo->GetGoType() != GAMEOBJECT_TYPE_DOOR)
+    {
+        PSendSysMessage("This command can be used only on doors");
+        return false;
+    }
+
     char* po = strtok(NULL, " ");
-    int iopen = (int)atof(po);
+    int iopen = po ? (int)atof(po) : 0;
 
     if (iopen && iopen != 1)
         return false;
