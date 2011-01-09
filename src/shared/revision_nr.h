89771658dd5892222e0e4665eb597e8d73add530
Tassadar <vbocek@gmail.com>
2011-01-09 13:36:44 +0100

    [pr1038] Remove negative auras on bg end

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10707"
- #define REVISION_VP "pr1037"
+ #define REVISION_VP "pr1038"
  #define REVISION_VP_DATE "9.1.2011"
 #endif // __REVISION_NR_H__
