608577a3f82b5a17a3c9b1240359f9a84f494661
KillerFrca <killerfrca@himym.cz>
2011-01-12 14:08:41 +0100

    BEKPORT !!!! [10990] Fix wrong threat assignment in Spell::EffectHealPct when m_caster and m_originalCaster differ.
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3717,7 +3717,7 @@ void Spell::EffectHealPct(SpellEffectIndex /*eff_idx*/)
         unitTarget->CalculateHealAbsorb(addhealth, &absorb);
 
         int32 gain = caster->DealHeal(unitTarget, addhealth - absorb, m_spellInfo, false, absorb);
-        unitTarget->getHostileRefManager().threatAssist(m_caster, float(gain) * 0.5f, m_spellInfo);
+        unitTarget->getHostileRefManager().threatAssist(caster, float(gain) * 0.5f, m_spellInfo);
     }
 }
 
