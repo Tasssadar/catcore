83c6f6fbbccebaf6cfa4418243589c86e72a489b
Lynx3d <lynx3d@some-imaginary-isp.org>
2011-02-23 11:50:43 +0300

    [11199] Resolve scripting library load problem at FreeBSD with cmake
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "11062"
+ #define REVISION_NR "11199"
  #define REVISION_VP "pr1076"
  #define REVISION_VP_DATE "25.2.2011"
 #endif // __REVISION_NR_H__
