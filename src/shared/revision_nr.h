efb4f72fdb202e4d4ea227542ce865e8d0625e92
Tassadar <vbocek@gmail.com>
2011-04-30 14:36:22 +0200

    [pr1107] Little clean-up in lfg code

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "11199"
- #define REVISION_VP "pr1106"
- #define REVISION_VP_DATE "10.4.2011"
+ #define REVISION_VP "pr1107"
+ #define REVISION_VP_DATE "30.4.2011"
 #endif // __REVISION_NR_H__
