c6d403e0e7c3399bd63c8a4c9d847a8ea2b07153
Tassadar <vbocek@gmail.com>
2011-06-17 16:37:55 +0200

    [pr1125] Fix Glyph of Thunderstorm

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "11333"
- #define REVISION_VP "pr1121"
+ #define REVISION_VP "pr1125"
  #define REVISION_VP_DATE "17.6.2011"
 #endif // __REVISION_NR_H__
