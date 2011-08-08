25b1ac36e1f051794e266542a134b59e1b2b7353
KillerFrca <killerfrca@gmail.com>
2011-08-08 11:11:36 +0200

    [pr1182] Vyhledavani creatury v mapě pomoci uint64 guidu
    Signed-off-by: KillerFrca <killerfrca@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
- #define REVISION_VP "pr1181"
+ #define REVISION_VP "pr1182"
  #define REVISION_SP "vsd263"
  #define REVISION_VP_DATE "5.8.2011"
 #endif // __REVISION_NR_H__
