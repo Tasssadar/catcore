82e923035e249570456e185ee09d2737503d6da0
Tassadar <vbocek@gmail.com>
2011-08-10 15:26:00 +0200

    [pr1192][vsd269] Rename UNIT_BYTE1_FLAG_UNK_2 to UNIT_BYTE1_FLAG_FLY_ANIM, fix cyanigosa
    Conflicts:
    
    	src/scripts/northrend/nexus/eye_of_eternity/boss_malygos.cpp
    	src/shared/revision_nr.h

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
- #define REVISION_VP "pr1190"
- #define REVISION_SP "vsd268"
- #define REVISION_VP_DATE "9.8.2011"
+ #define REVISION_VP "pr1192"
+ #define REVISION_SP "vsd269"
+ #define REVISION_VP_DATE "10.8.2011"
 #endif // __REVISION_NR_H__
