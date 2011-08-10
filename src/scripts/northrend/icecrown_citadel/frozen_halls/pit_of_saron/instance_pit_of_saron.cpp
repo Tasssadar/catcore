def4d51ad962baffdea7d99a852abaf3d3656cd3
Tassadar <vbocek@gmail.com>
2011-08-10 23:13:54 +0200

    yeah, new files

---
+++ src/scripts/northrend/icecrown_citadel/frozen_halls/pit_of_saron/instance_pit_of_saron.cpp
@@ -0,0 +1,181 @@
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* ScriptData
+SDName: instance_ahnkahet
+SD%Complete: 0
+SDComment:
+SDCategory: Ahn'kahet
+EndScriptData */
+
+#include "precompiled.h"
+#include "pit_of_saron.h"
+
+struct MANGOS_DLL_DECL instance_pit_of_saron : public ScriptedInstance
+{
+    instance_pit_of_saron(Map* pMap) : ScriptedInstance(pMap) {Initialize();};
+
+    uint32 m_auiEncounter[MAX_ENCOUNTER];
+    std::string strInstData;
+
+    uint64 m_uiGarfrostGUID;
+    uint64 m_uiIckGUID;
+    uint64 m_uiTyrannusGUID;
+    uint64 m_uiIceWallGUID;
+
+    void Initialize()
+    {
+        memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+
+        m_uiGarfrostGUID = 0;
+        m_uiIckGUID = 0;
+        m_uiTyrannusGUID = 0;
+        m_uiIceWallGUID = 0;
+    }
+
+    void OnCreatureCreate(Creature* pCreature)
+    {
+        switch(pCreature->GetEntry())
+        {
+            case NPC_GARFROST:            m_uiGarfrostGUID = pCreature->GetGUID(); break;
+            case NPC_TYRANNUS:            m_uiTyrannusGUID = pCreature->GetGUID(); break;
+            case NPC_ICK:
+            {
+                float x, y, z;
+                pCreature->GetPosition(x, y, z);
+                pCreature->SetVisibility(VISIBILITY_OFF);
+                pCreature->ForcedDespawn();
+                if (m_auiEncounter[1] == DONE)
+                    break;
+                Vehicle *pIck = pCreature->SummonVehicle(NPC_ICK, x, y, z, pCreature->GetOrientation(), 522, NULL, 0);
+                pIck->SetRespawnDelay(86400);
+                m_uiIckGUID = pIck->GetGUID();
+                break;
+            }
+        }
+    }
+
+    void OnObjectCreate(GameObject* pGo)
+    {
+        switch(pGo->GetEntry())
+        {
+            case GO_ICE_WALL:
+                m_uiIceWallGUID = pGo->GetGUID();
+                if (m_auiEncounter[1] == DONE && m_auiEncounter[0] == DONE)
+                    DoUseDoorOrButton(m_uiIceWallGUID);
+                break;
+        }
+    }
+
+    void SetData(uint32 uiType, uint32 uiData)
+    {
+        debug_log("SD2: Instance Ahn'Kahet: SetData received for type %u with data %u",uiType,uiData);
+
+        switch(uiType)
+        {
+            case TYPE_GARFROST:
+                m_auiEncounter[0] = uiData;
+                if (uiData == DONE && m_auiEncounter[1] = DONE)
+                    DoUseDoorOrButton(m_uiIceWallGUID);
+                break;
+            case TYPE_ICK_AND_CRICK:
+                m_auiEncounter[1] = uiData;
+                if (uiData == DONE && m_auiEncounter[0] = DONE)
+                    DoUseDoorOrButton(m_uiIceWallGUID);
+                break;
+            case TYPE_TYRANNUS:
+                m_auiEncounter[2] = uiData;
+                break;
+            default:
+                error_log("SD2: Instance Pit of Saron: ERROR SetData = %u for type %u does not exist/not implemented.",uiType,uiData);
+                break;
+        }
+
+        if (uiData == DONE)
+        {
+            OUT_SAVE_INST_DATA;
+
+            std::ostringstream saveStream;
+            saveStream << m_auiEncounter[0] << " " << m_auiEncounter[1] << " " << m_auiEncounter[2] << " ";
+
+            strInstData = saveStream.str();
+
+            SaveToDB();
+            OUT_SAVE_INST_DATA_COMPLETE;
+        }
+    }
+
+    const char* Save()
+    {
+        return strInstData.c_str();
+    }
+
+    void Load(const char* chrIn)
+    {
+        if (!chrIn)
+        {
+            OUT_LOAD_INST_DATA_FAIL;
+            return;
+        }
+
+        OUT_LOAD_INST_DATA(chrIn);
+
+        std::istringstream loadStream(chrIn);
+        loadStream >> m_auiEncounter[0] >> m_auiEncounter[1] >> m_auiEncounter[2];
+
+        for(uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+        {
+            if (m_auiEncounter[i] == IN_PROGRESS)
+                m_auiEncounter[i] = NOT_STARTED;
+        }
+
+        OUT_LOAD_INST_DATA_COMPLETE;
+    }
+
+    uint32 GetData(uint32 uiType)
+    {
+        switch(uiType)
+        {
+            case TYPE_GARFROST:
+                return m_auiEncounter[0];
+            case TYPE_ICK_AND_CRICK:
+                return m_auiEncounter[1];
+            case TYPE_TYRANNUS:
+                return m_auiEncounter[2];
+        }
+        return 0;
+    }
+
+    uint64 GetData64(uint32 uiData)
+    {
+        return 0;
+    }
+};
+
+InstanceData* GetInstanceData_instance_pit_of_saron(Map* pMap)
+{
+    return new instance_pit_of_saron(pMap);
+}
+
+void AddSC_instance_pit_of_saron()
+{
+    Script* newscript;
+
+    newscript = new Script;
+    newscript->Name = "instance_pit_of_saron";
+    newscript->GetInstanceData = &GetInstanceData_instance_pit_of_saron;
+    newscript->RegisterSelf();
+}
---
+++ src/scripts/northrend/icecrown_citadel/frozen_halls/pit_of_saron/pit_of_saron.h
@@ -0,0 +1,20 @@
+#ifndef PIT_OF_SARON_H
+#define PIT_OF_SARON_H
+
+enum
+{
+    MAX_ENCOUNTER      = 3,
+
+    TYPE_GARFROST      = 0,
+    TYPE_ICK_AND_KRICK = 1,
+    TYPE_TYRANNUS      = 2,
+    
+    NPC_GARFROST       = 36494,
+    NPC_ICK            = 36476,
+    NPC_KRICK          = 36477,
+    NPC_TYRANNUS       = 36658,
+    NPC_RIMEFANG       = 36661,
+
+    GO_ICE_WALL        = 201885,
+}
+#endif
