1b151bb4146625a4b0b6ac7b138509c049c3953b
Tassadar <vbocek@gmail.com>
2011-08-16 11:21:04 +0200

    revision

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
- #define REVISION_VP "pr1208"
+ #define REVISION_VP "pr1210"
  #define REVISION_SP "vsd289"
- #define REVISION_VP_DATE "15.8.2011"
+ #define REVISION_VP_DATE "16.8.2011"
 #endif // __REVISION_NR_H__
