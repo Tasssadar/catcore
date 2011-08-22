7280d6e70338fb380e7dfc6a873e323087a87cb8
KillerFrca <killerfrca@gmail.com>
2011-08-22 12:26:50 +0200

    [vsd309] Metoda na despawnovani všech mrdkovin v dosahu, moznost upresneni jakej typ mazat jen
    Signed-off-by: KillerFrca <killerfrca@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -2,6 +2,6 @@
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
  #define REVISION_VP "pr1237"
- #define REVISION_SP "vsd308"
+ #define REVISION_SP "vsd309"
  #define REVISION_VP_DATE "21.8.2011"
 #endif // __REVISION_NR_H__
