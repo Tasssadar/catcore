1f361ff63adc6fae8e463080d9548511b7be9067
KillerFrca <killerfrca@gmail.com>
2011-08-23 12:47:53 +0200

    [vsd311] Ten posledni zmrd snad hotovej
    Signed-off-by: KillerFrca <killerfrca@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -2,6 +2,6 @@
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
  #define REVISION_VP "pr1238"
- #define REVISION_SP "vsd310"
+ #define REVISION_SP "vsd311"
  #define REVISION_VP_DATE "23.8.2011"
 #endif // __REVISION_NR_H__
