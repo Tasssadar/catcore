43fd4f2c9adfbdeb7a733310d01a38b5075eeeff
Tassadar <vbocek@gmail.com>
2011-08-28 14:54:00 +0200

    [pr1258] Show average item level in .s info

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
- #define REVISION_VP "pr1257"
+ #define REVISION_VP "pr1258"
  #define REVISION_SP "vsd328"
- #define REVISION_VP_DATE "25.8.2011"
+ #define REVISION_VP_DATE "28.8.2011"
 #endif // __REVISION_NR_H__
