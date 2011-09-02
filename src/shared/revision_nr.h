88d3faf8faf59856d492a7da403c96011fd7ddd9
Tassadar <vbocek@gmail.com>
2011-09-02 23:47:59 +0200

    [vsd330] Move krick out from dead body of ick in Pit of Saron

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -2,6 +2,6 @@
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
  #define REVISION_VP "pr1267"
- #define REVISION_SP "vsd329"
+ #define REVISION_SP "vsd330"
  #define REVISION_VP_DATE "2.9.2011"
 #endif // __REVISION_NR_H__
