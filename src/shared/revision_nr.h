77384a64b7dc306d0b648fb8ca601503ae1087b5
Tassadar <vbocek@gmail.com>
2011-09-27 15:29:15 +0200

    [pr1315] Backport "[11805] Send unit movement speeds in the proper order, correct baseMoveSpeed value order"

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
- #define REVISION_VP "p1314"
+ #define REVISION_VP "pr1315"
  #define REVISION_SP "vsd382"
- #define REVISION_VP_DATE "26.9.2011"
+ #define REVISION_VP_DATE "27.9.2011"
 #endif // __REVISION_NR_H__
