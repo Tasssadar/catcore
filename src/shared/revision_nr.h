14b0c033d8412fad32ce962339cc11368fca689f
Tassadar <vbocek@gmail.com>
2011-11-19 13:05:56 +0100

    [vsd390] Properly init Ebon Gargoyle's target to prevent crash
    Conflicts:
    
    	src/shared/revision_nr.h

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "11543"
- #define REVISION_VP "pr1327"
- #define REVISION_SP "vsd386"
- #define REVISION_VP_DATE "17.11.2011"
+ #define REVISION_VP "pr1332"
+ #define REVISION_SP "vsd390"
+ #define REVISION_VP_DATE "19.11.2011"
 #endif // __REVISION_NR_H__
