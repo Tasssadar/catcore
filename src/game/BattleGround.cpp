785686710f44e7fab04c780042701b1da4c42a20
KillerFrca <killerfrca@gmail.com>
2011-11-20 15:03:52 +0100

    [pr1339] V arena logu tahani ip z pameti a ne z db
    Signed-off-by: KillerFrca <killerfrca@gmail.com>

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -2225,9 +2225,10 @@ void ArenaLog::writeMember(Player *plr, uint8 ratingChange, bool win)
     if (++count <= arenaType)
     {
         // get ip
-        QueryResult *result = LoginDatabase.PQuery("SELECT last_ip FROM account WHERE id in (SELECT account FROM characters.characters WHERE guid = %u)", plr->GetGUIDLow());
-        const char* ip = result ? result->Fetch()[0].GetString() : "IP not found";
-        delete result;
+        //QueryResult *result = LoginDatabase.PQuery("SELECT last_ip FROM realmd.account WHERE id in (SELECT account FROM characters.characters WHERE guid = %u)", plr->GetGUIDLow());
+        //const char* ip = result ? result->Fetch()[0].GetString() : "IP not found";
+        //delete result;
+        const char* ip = plr->GetSession()->GetRemoteAddress().c_str();
 
         std::stringstream& side = win ? TxtWinner : TxtLoser;
 
