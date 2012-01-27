936e907da517a69bf3885c88d83dcaa041b50ad5
Tassadar <vbocek@gmail.com>
2012-01-27 20:38:14 +0100

    PCH under GCC v4.6

--- cmake/FindPCHSupport.cmake
+++ cmake/FindPCHSupport.cmake
@@ -16,8 +16,10 @@ IF(CMAKE_COMPILER_IS_GNUCXX)
       ${CMAKE_CXX_COMPILER}
         ARGS  ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
         OUTPUT_VARIABLE gcc_compiler_version)
-    #MESSAGE("GCC Version: ${gcc_compiler_version}")
+    MESSAGE("GCC Version: ${gcc_compiler_version}")
     IF(gcc_compiler_version MATCHES "4\\.[0-9]\\.[0-9]")
+        SET(PCHSupport_FOUND TRUE)
+    ELSE(gcc_compiler_version MATCHES "4\\.[0-9]")
         SET(PCHSupport_FOUND TRUE)
     ELSE(gcc_compiler_version MATCHES "4\\.[0-9]\\.[0-9]")
         IF(gcc_compiler_version MATCHES "3\\.4\\.[0-9]")
